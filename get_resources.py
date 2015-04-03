1cd81389704a3f08487de396500242724c18d46d
Stavitsky A <alexandr.stavitsky@gmail.com>
2015-04-03 10:55:13 +0300

    add yield returning in get_resources()

--- get_resources.py
+++ get_resources.py
@@ -3,6 +3,68 @@ from ceilometer_local_lib import apply_metaquery_filter
 from ceilometer_local_lib import PoolConnection
 from ceilometer_local_lib import Object
 from dateutil import parser
+import six  # only for Base class work
+# from ceilometer.storage import models
+
+# Remove classes on controller, just for tests
+# Shold import Resource class from models.Resource
+
+
+class Model(object):
+
+    """Base class for storage API models."""
+
+    def __init__(self, **kwds):
+        self.fields = list(kwds)
+        for k, v in six.iteritems(kwds):
+            setattr(self, k, v)
+
+    def as_dict(self):
+        d = {}
+        for f in self.fields:
+            v = getattr(self, f)
+            if isinstance(v, Model):
+                v = v.as_dict()
+            elif isinstance(v, list) and v and isinstance(v[0], Model):
+                v = [sub.as_dict() for sub in v]
+            d[f] = v
+        return d
+
+    def __eq__(self, other):
+        return self.as_dict() == other.as_dict()
+
+    @classmethod
+    def get_field_names(cls):
+        fields = inspect.getargspec(cls.__init__)[0]
+        return set(fields) - set(["self"])
+
+
+class Resource(Model):
+
+    """Something for which sample data has been collected."""
+
+    def __init__(self, resource_id, project_id,
+                 first_sample_timestamp,
+                 last_sample_timestamp,
+                 source, user_id, metadata):
+        """Create a new resource.
+        :param resource_id: UUID of the resource
+        :param project_id:  UUID of project owning the resource
+        :param first_sample_timestamp: first sample timestamp captured
+        :param last_sample_timestamp: last sample timestamp captured
+        :param source:      the identifier for the user/project id definition
+        :param user_id:     UUID of user owning the resource
+        :param metadata:    most current metadata for the resource (a dict)
+        """
+        Model.__init__(self,
+                       resource_id=resource_id,
+                       first_sample_timestamp=first_sample_timestamp,
+                       last_sample_timestamp=last_sample_timestamp,
+                       project_id=project_id,
+                       source=source,
+                       user_id=user_id,
+                       metadata=metadata,
+                       )
 
 
 def get_resources(user=None, project=None, source=None,
@@ -42,7 +104,8 @@ def get_resources(user=None, project=None, source=None,
 
     subq_values = []
     samples_subq = ("SELECT resource_id, source_id, user_id, project_id,"
-                    " max(timestamp) as max_ts, min(timestamp) as min_ts"
+                    " max(timestamp) as max_ts, min(timestamp) as min_ts,"
+                    " metadata"
                     " FROM samples")
 
     if s_filter.resource:
@@ -56,7 +119,7 @@ def get_resources(user=None, project=None, source=None,
             samples_subq += " AND resource_id = %s"
             subq_values.append(resource_id)
         else:
-            return tuple()
+            yield tuple()
 
     if s_filter.user:
         user_id_q = ("SELECT id FROM users"
@@ -69,7 +132,7 @@ def get_resources(user=None, project=None, source=None,
             samples_subq += " AND user_id = %s"
             subq_values.append(user_id)
         else:
-            return tuple()
+            yield tuple()
 
     if s_filter.project:
         project_id_q = ("SELECT id FROM projects"
@@ -82,7 +145,7 @@ def get_resources(user=None, project=None, source=None,
             samples_subq += " AND project_id = %s"
             subq_values.append(project_id)
         else:
-            return tuple()
+            yield tuple()
 
     if s_filter.source:
         source_name_q = ("SELECT id FROM sources"
@@ -95,7 +158,7 @@ def get_resources(user=None, project=None, source=None,
             samples_subq += " AND source_id = %s"
             subq_values.append(source_id)
         else:
-            return tuple()
+            yield tuple()
 
     if s_filter.start:
         ts_start = s_filter.start
@@ -118,13 +181,14 @@ def get_resources(user=None, project=None, source=None,
         samples_subq += " AND {}".format(q)
         subq_values.append(v)
 
-    samples_subq += " GROUP BY resource_id, source_id, user_id, project_id"
+    samples_subq += (" GROUP BY resource_id, source_id,"
+                     " user_id, project_id, metadata")
 
     samples_subq = samples_subq.replace(" AND", " WHERE", 1)
 
     query = ("SELECT resources.resource_id as id, sources.name as source_name,"
              " users.uuid as user_id, projects.uuid as project_id,"
-             " max_ts, min_ts"
+             " max_ts, min_ts, metadata"
              " FROM ({}) as samples"
              " JOIN resources ON samples.resource_id = resources.id"
              " JOIN users ON samples.user_id = users.id"
@@ -136,21 +200,19 @@ def get_resources(user=None, project=None, source=None,
         cur.execute(query, subq_values)
         resp = cur.fetchall()
 
-    print "----------------------"
-    print("QUERY\n")
-    print query
-    print "----------------------"
-    print("VALUES\n")
-    print subq_values
-    print "----------------------"
-    print("RESPONCE\n")
-    print resp
-    print "----------------------"
-    i = 0
     for res in resp:
-        i += 1
-        print "\nRecord #{}:".format(i)
-        print res
+        for j in range(0, 7):
+            print res[j]
+            print '\n'
+
+        yield Resource(
+            resource_id=res[0],
+            user_id=res[2],
+            project_id=res[3],
+            first_sample_timestamp=res[4],
+            last_sample_timestamp=res[5],
+            source=res[1],
+            metadata=res[6])
 
 metaquery = {'metadata.status': 'active',
              'metadata.memory_mb': 512,
@@ -159,8 +221,9 @@ metaquery = {'metadata.status': 'active',
 
 user_id_1 = "0c76126d-a5e3-4503-be44-4d9bed645cf7"
 user_id_2 = "3d622ea5-a70a-42d3-aae5-49ddfc1ef355"
-dt_start = parser.parse("2015-03-16 12:51:50")
+dt_start = parser.parse("2015-03-16 12:51:52")
 dt_end = parser.parse("2015-03-16 14:41:52")
-get_resources(user=user_id_2,
-              metaquery=metaquery)
-print "----------------------"
+for resource in get_resources(user=user_id_2,
+                              metaquery=metaquery,
+                              start_timestamp=dt_start):
+    print resource
